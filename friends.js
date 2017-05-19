2ebaac58c695421083b2790e03c80a02c444b38e
yaojenkuo <tonykuoyj@gmail.com>
2017-05-20 00:45:57 +0800

    first commit

---
+++ friends.js
@@ -0,0 +1,11 @@
+var friends = {
+	"genre": "Sitcom",
+	"seasons": 10,
+	"episodes": 236,
+	"stars": ["Jennifer Aniston", "Courteney Cox", "Lisa Kudrow", "Matt LeBlanc", "Matthew Perry", "David Schwimmer"]
+};
+
+// 印出我們想知道的資訊
+console.log("六人行共有幾季：" + friends["seasons"]);
+console.log("六人行共有幾集：" + friends["episodes"]);
+console.log("誰飾演 Chandler Bing：" + friends["stars"][4]);
